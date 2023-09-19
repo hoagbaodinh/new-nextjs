796e909de2795b858852f1f6d175c59286ce3110
hoagbaodinh <46434823+hoagbaodinh@users.noreply.github.com>
2023-09-19 17:54:58 +0700

    fixed fallback

--- src/pages/[meetupId]/index.js
+++ src/pages/[meetupId]/index.js
@@ -31,7 +31,7 @@ export const getStaticPaths = async () => {
 
   client.close();
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
