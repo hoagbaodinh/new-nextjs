e364bea47ee35985d82d6d379faf23554795a08b
hoagbaodinh <46434823+hoagbaodinh@users.noreply.github.com>
2023-09-19 17:40:57 +0700

    first-commit

---
+++ src/pages/new-meetup/index.js
@@ -0,0 +1,34 @@
+import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+const NewMeetupPage = () => {
+  const router = useRouter();
+
+  const addMeetupHandler = async (enteredData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
+  };
+  return (
+    <>
+      <Head>
+        <title>Add New Meetup</title>
+        <meta name="description" content="Add a new Meetup" />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+    </>
+  );
+};
+
+export default NewMeetupPage;
